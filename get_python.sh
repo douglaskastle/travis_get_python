66c7c84ec2e13c84eaeac75f07a1e38f969e30e8
Douglas Kastle <douglas.kastle@gmail.com>
2019-11-06 14:37:53 +0000

    Restart work

--- get_python.sh
+++ get_python.sh
@@ -7,23 +7,18 @@ case "${unameOut}" in
     MSYS_NT*)   machine=MsysNt;;
     *)          machine="UNKNOWN:${unameOut}"
 esac
-#echo ${machine}
 
 PYTHON_VENV="${VENV_CACHE}/${machine}/Python-${PYTHON_REV}"
-PYTHON_INSTALL="${VENV_CACHE}/${machine}/local"
-#rm -rf "${VENV_CACHE}/${machine}"
+
 mkdir -p "${VENV_CACHE}/${machine}"
 ls "${VENV_CACHE}/${machine}"
 
 if [ ${machine} == "MsysNt" ]; then
-    echo "It's Windows"
     choco install python --version ${PYTHON_REV}
     py -m venv --copies ${PYTHON_VENV}
 else
    if [ ! -d "${PYTHON_VENV}" ]; then
-        echo "False"
         cd $TRAVIS_BUILD_DIR
-        echo "It's other"
         if [ ${machine} == "Mac" ]; then
             #brew update
             brew upgrade openssl
@@ -34,27 +29,20 @@ else
         tar -zxvf Python-${PYTHON_REV}.tgz > logfile 2>&1
         cd Python-${PYTHON_REV}
         if [ ${machine} == "Mac" ]; then
-            ./configure --prefix=${PYTHON_INSTALL}/Python-${PYTHON_REV} > logfile 2>&1
+            ./configure > logfile 2>&1
             make > logfile 2>&1
-            #make altinstall > logfile 2>&1
             ./python.exe -m venv --copies ${PYTHON_VENV}
         else
-            ./configure --prefix=${PYTHON_INSTALL}/Python-${PYTHON_REV} > logfile 2>&1
+            ./configure > logfile 2>&1
             make > logfile 2>&1
-            #make altinstall
-            #${PYTHON_INSTALL}/Python-${PYTHON_REV}/bin/python3 -m venv --copies ${PYTHON_VENV}
             ./python -m venv --copies ${PYTHON_VENV}
         fi
-    else
-        echo "True"
     fi
 fi
 
 if [ ${machine} == "MsysNt" ]; then
-    ls ${PYTHON_VENV}/Scripts
     source ${PYTHON_VENV}/Scripts/activate
 else
-    ls ${PYTHON_VENV}/bin
     source ${PYTHON_VENV}/bin/activate
 fi
 
@@ -70,4 +58,3 @@ else
     echo "This is the expected version of Python:"
     echo ${PYTHON_RET}
 fi
-#ls $VENV_CACHE
