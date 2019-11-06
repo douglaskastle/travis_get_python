b91207e13b19d223c41cc54946d5e517d7b0cd74
Douglas Kastle <douglas.kastle@gmail.com>
2019-11-06 11:54:18 +0000

    Restart work

--- get_python.sh
+++ get_python.sh
@@ -11,17 +11,12 @@ echo ${machine}
 
 PYTHON_REV="3.7.4"
 mkdir -p $TRAVIS_BUILD_DIR/local
-mkdir -p $TRAVIS_BUILD_DIR/.venv
 if [ ${machine} == "MsysNt" ]; then
     echo "It's Windows"
-    ls $VENV_CACHE
     cd $TRAVIS_BUILD_DIR
     choco install python --version ${PYTHON_REV}
-    py -m venv $TRAVIS_BUILD_DIR/.venv/Python-${PYTHON_REV}
-    source $TRAVIS_BUILD_DIR/.venv/Python-${PYTHON_REV}/Scripts/activate
     py -m venv $VENV_CACHE/Python-${PYTHON_REV}
-    ls $VENV_CACHE
-    ls $VENV_CACHE/Python-${PYTHON_REV}/Scripts
+    source $VENV_CACHE/Python-${PYTHON_REV}/Scripts/activate
 else
     echo "It's other"
     if [ ${machine} == "Mac" ]; then
@@ -33,16 +28,17 @@ else
     wget https://www.python.org/ftp/python/${PYTHON_REV}/Python-${PYTHON_REV}.tgz
     tar -zxvf Python-${PYTHON_REV}.tgz
     cd Python-${PYTHON_REV}
-    ./configure --prefix=$TRAVIS_BUILD_DIR/local/Python-${PYTHON_REV}
+    ./configure
     make
-    ls
+    #ls
     if [ ${machine} == "Mac" ]; then
-        ./python.exe -m venv $TRAVIS_BUILD_DIR/.venv/Python-${PYTHON_REV}
+        ./python.exe -m venv $VENV_CACHE/Python-${PYTHON_REV}
     else
-        ./python -m venv $TRAVIS_BUILD_DIR/.venv/Python-${PYTHON_REV}
+        ./python -m venv $VENV_CACHE/Python-${PYTHON_REV}
     fi
-    source $TRAVIS_BUILD_DIR/.venv/Python-${PYTHON_REV}/bin/activate
+    source $VENV_CACHE/Python-${PYTHON_REV}/bin/activate
 fi
 python -m pip install pip yolk3k --upgrade
 python -m yolk -l
 which python
+ls $VENV_CACHE
