2bf1797f5612343276d78ba5ae52e1130008c039
Douglas Kastle <douglas.kastle@gmail.com>
2019-11-08 16:52:54 +0000

    Restart work

--- get_python.sh
+++ get_python.sh
@@ -10,7 +10,7 @@ esac
 
 PYTHON_VENV="${VENV_CACHE}/${machine}/Python-${PYTHON_REV}"
 
-rm -rf  "${VENV_CACHE}/${machine}"
+#rm -rf  "${VENV_CACHE}/${machine}"
 #rm -rf  "${VENV_CACHE}/Mac"
 
 mkdir -p "${VENV_CACHE}/${machine}"
